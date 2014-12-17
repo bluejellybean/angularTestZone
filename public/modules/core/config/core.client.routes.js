64bbd34cdb16393fe2f97830b0d85fde43a0c361
Alex Barkell <alexbarkell@gmail.com>
2014-12-17 18:11:42 -0500

    Added in about section

--- public/modules/core/config/core.client.routes.js
+++ public/modules/core/config/core.client.routes.js
@@ -7,10 +7,15 @@ angular.module('core').config(['$stateProvider', '$urlRouterProvider',
 		$urlRouterProvider.otherwise('/');
 
 		// Home state routing
-		$stateProvider.
-		state('home', {
+		$stateProvider
+		.state('home', {
 			url: '/',
 			templateUrl: 'modules/core/views/home.client.view.html'
+		})
+
+		.state('about', {
+			url: '/about',
+			templateUrl: 'modules/core/views/about.client.view.html'
 		});
 	}
 ]);
