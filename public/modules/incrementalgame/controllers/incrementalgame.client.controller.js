c5db46d1abd9d3a3cd956221c81821478e7fa971
Alex Barkell <alexbarkell@gmail.com>
2014-12-19 02:34:49 -0500

    Cleaned up game controller

--- public/modules/incrementalgame/controllers/incrementalgame.client.controller.js
+++ public/modules/incrementalgame/controllers/incrementalgame.client.controller.js
@@ -3,105 +3,11 @@
 angular.module('incrementalgame').controller('IncrementalgameController', ['$scope', '$interval', 'Gamelogic',
  function($scope, $interval, Gamelogic) {
 
-    var Tiers = [
-      {
-        worker: {
-          name: 'worker 1',
-          price: 15,
-          description: 'was the first worker, gain 1 item per sec',
-        },
-        upgrade: [
-          {
-            name: 'upgradeTier1 #1',
-            price: 50,
-            description: 'Was the first upgrade, x2 worker1 production'
-          },
-          {
-            name: 'upgradeTier1 #2',
-            price: 100,
-            description: 'Was the second upgrade, x4 worker1 production'
-          }
-        ]
-      },
-      {
-        worker: {
-          name: 'worker 2',
-          price: 50,
-          description: 'was the first worker, gain 5 item per sec',
-        },
-        upgrade: [
-          {
-            name: 'upgradeTier2 #1',
-            price: 500,
-            description: 'Was the first upgrade, x2 worker2 production'
-          },
-          {
-            name: 'upgradeTier2 #2',
-            price: 1000,
-            description: 'Was the second upgrade, x4 worker2 production'
-          }
-        ]
-      },
-      {
-        worker: {
-          name: 'worker 3',
-          price: 100,
-          description: 'was the first worker, gain 3 item per sec',
-        },
-        upgrade: [
-          {
-            name: 'upgradeTier3 #1',
-            price: 50,
-            description: 'Was the first upgrade, x2 worker1 production'
-          },
-          {
-            name: 'upgradeTier3 #2',
-            price: 100,
-            description: 'Was the second upgrade, x4 worker1 production'
-          }
-        ]
-      },
-      {
-      worker: {
-          name: 'worker 4',
-          price: 500,
-          description: 'was the first worker, gain 3 item per sec',
-        },
-        upgrade: [
-          {
-            name: 'upgradeTier4 #1',
-            price: 50,
-            description: 'Was the first upgrade, x2 worker1 production'
-          },
-          {
-            name: 'upgradeTier4 #2',
-            price: 100,
-            description: 'Was the second upgrade, x4 worker1 production'
-          }
-        ]
-      },
-      {
-        worker: {
-          name: 'worker 5',
-          price: 1000,
-          description: 'was the first worker, gain 3 item per sec',
-        },
-        upgrade: [
-          {
-            name: 'upgradeTier5 #1',
-            price: 50,
-            description: 'Was the first upgrade, x2 worker1 production'
-          },
-          {
-            name: 'upgradeTier5 #2',
-            price: 100,
-            description: 'Was the second upgrade, x4 worker1 production'
-          }
-        ]
-      },
-    ];
+
 
     this.userInfo = Gamelogic.getUserInformation();
+    var Tiers = Gamelogic.getTiers();
+  
     this.level = Tiers;
 
     this.increaseTotalClicks = function() {
@@ -169,25 +75,35 @@ angular.module('incrementalgame').controller('IncrementalgameController', ['$sco
       angular.forEach(workers, function(value, key) {
         
         if( key === 0 ) {
+
           workerCount = Gamelogic.getWorkerCount(key);
           increaseValue = workerCount * 1;
           Gamelogic.increaseMoneyBy(increaseValue);
+
         } else if ( key === 1 ) {
+
           workerCount = Gamelogic.getWorkerCount(key);
           increaseValue = workerCount * 5;
           Gamelogic.increaseMoneyBy(increaseValue);
+
         } else if ( key === 2 ) {
+
           workerCount = Gamelogic.getWorkerCount(key);
           increaseValue = workerCount * 25;
           Gamelogic.increaseMoneyBy(increaseValue);
+
         } else if ( key === 3 ) {
+
           workerCount = Gamelogic.getWorkerCount(key);
           increaseValue = workerCount * 50;
           Gamelogic.increaseMoneyBy(increaseValue);
+
         } else if ( key === 4 ) {
+
           workerCount = Gamelogic.getWorkerCount(key);
           increaseValue = workerCount * 75;
           Gamelogic.increaseMoneyBy(increaseValue);
+          
         }
 
       });
